2a2feaf6065827b5d64db844d5845763d5e49ad3
Chin <justinfchin@gmail.com>
2017-04-06 18:53:20 -0400

    Added 'static' folder

--- app/_init_.py
+++ app/_init_.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template #import class
-app = Flask(__name__) 	#create instance of class
+from flask import Flask, render_template  # import class
+app = Flask(__name__)  # create instance of class
 
 app.config["DATABASE"] = 'losquatroamigos.db'
 app.config['DEBUG'] = True
 
-#don't know what this stuff is for.
-#app.config["SECRET_KEY"] = "like I'd tell you"
-#app.config["USERNAME"] = "Eric"
-#app.config["PASSWORD"] = "Schles"
+# don't know what this stuff is for.
+# app.config["SECRET_KEY"] = "like I'd tell you"
+# app.config["USERNAME"] = "Eric"
+# app.config["PASSWORD"] = "Schles"
 
-#Chin - commented out the bottom
-#from app import views, models
+# Chin - commented out the bottom
+# from app import views, models
 
-@app.route("/")	#tells Flask what URL should trigger our function
+@app.route("/")  # tells Flask what URL should trigger our function
 def main():
-	return render_template('index.html')
-	
+    return render_template('index.html')
+
 if __name__ == "__main__":
-	app.run()
+    app.run()
