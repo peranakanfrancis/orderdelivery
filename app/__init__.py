d2306cd98e3e2159462c155221ea617f1087c6b3
Ammar Karim <Ammar Karim>
2017-04-10 19:04:54 -0400

    add views and moved updated templates

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,7 @@
 This is the main home page
 ============================
 """
-
-# Third Party Imports
 from flask import Flask
-from flask import render_template  # for html
-from flask import request  # for reading posted values
 
 # Create Instance of Class
 app = Flask(__name__)
@@ -15,49 +11,4 @@ app = Flask(__name__)
 app.config["DATABASE"] = 'losquatroamigos.db'
 app.config['DEBUG'] = True
 
-
-# Run HomePage
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# Run MenuPage
-@app.route('/menu/')
-def showMenu():
-    return render_template('Menu.html')
-
-# Run SignUpPage
-@app.route('/showSignUp/')
-def showSignUp():
-    return render_template('signup.html')
-
-# Run Register
-@app.route('/signUp/')
-def signUp():
-
-    #read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
-
-    #validate the received values
-   # if _name and _email and _password
-
-@app.errorhandler(404)
-def PageNotFound(error):
-    return render_template('errors/404.html'), 404
-
-
-#Import all of our routes from routes.py
-    #from routes import *;
-
-#For Dynamic Pages Per User
-    #name is generated with the dynamic argument
-#@app.route('/user/<name>'):
-#def user(name):
-    #return '<h1>Hey, %s!</h1>' % name
-
-
-#Develop Web Server
-if __name__ == "__main__":
-    app.run(debug=True)
+from app import views, models
---
+++ app/views.py
@@ -0,0 +1,50 @@
+from app import app
+from flask import render_template,redirect, request, flash,g,session,url_for
+from models import *
+
+
+# Run HomePage
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Run MenuPage
+@app.route('/menu/')
+def showMenu():
+    return render_template('Menu.html')
+
+# Run SignUpPage
+@app.route('/showSignUp/')
+def showSignUp():
+    return render_template('signup.html')
+
+# Run Register
+@app.route('/signUp/')
+def signUp():
+
+    #read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    #validate the received values
+   # if _name and _email and _password
+
+@app.errorhandler(404)
+def PageNotFound(error):
+    return render_template('errors/404.html'), 404
+
+
+#Import all of our routes from routes.py
+    #from routes import *;
+
+#For Dynamic Pages Per User
+    #name is generated with the dynamic argument
+#@app.route('/user/<name>'):
+#def user(name):
+    #return '<h1>Hey, %s!</h1>' % name
+
+
+#Develop Web Server
+if __name__ == "__main__":
+    app.run(debug=True)
