c90bb5da3c854644d2ae7bf26b1c3c555e8c6ef4
Ammar Karim <ammark1111@gmail.com>
2017-04-11 20:35:22 -0400

    edit customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -7,12 +7,6 @@ def insert_account_holder(email,username,phone,password):
         con.commit()
 """
 
-def add_money(user, amount_to_add):
-    with sql.connect("database.db") as con:
-        cur = con.cursor()
-        cur.execute("UPDATE users SET acc_funds = acc_funds + amount_to_add WHERE user_id = user")
-        con.commit()
-
 def delete_account(user):
     with sql.connect("database.db") as con:
         cur = con.cursor()
